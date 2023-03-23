7ebed3a9359b3a41bd28887451c2af8e4e8a3ee4
DongwonKim <rnforqor27@naver.com>
2023-03-23 16:35:41 +0900

    first commit

---
+++ Bookmanagement/src/Test/Pushtest.java
@@ -0,0 +1,10 @@
+package Test;
+
+public class Pushtest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("test for github");
+	}
+
+}
---
+++ Bookmanagement/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author rnfor
+ *
+ */
+module Bookmanagement {
+}

caa00628cfdf0432a82b00f70d24d314d09239de
DongwonKim <rnforqor27@naver.com>
2023-04-14 16:30:07 +0900

    Book and Book Management

--- Bookmanagement/src/Bookmanagement1/MenuManagement.java
+++ Bookmanagement/src/Bookmanagement1/MenuManagement.java
@@ -15,15 +15,14 @@ public static void main(String[] args) {
 		}
 		
 		
-		while (num !=6) {
+		while (num !=5) {
 			System.out.println("### Book Management System Menu ###");
 			System.out.println("1. Add books");
 			System.out.println("2. Delete books");
 			System.out.println("3. Edit books");
 			System.out.println("4. View books");
-			System.out.println("5. Show a menu");
-			System.out.println("6. Exit");
-			System.out.println("select one number between 1 to 6: ");
+			System.out.println("5. Exit");
+			System.out.println("select one number between 1 to 5: ");
 			num = input.nextInt();
 					
 			if (num==1){					
@@ -43,9 +42,6 @@ public static void main(String[] args) {
 			}
 		}
 	}
-		
-		
-
 }
 
  

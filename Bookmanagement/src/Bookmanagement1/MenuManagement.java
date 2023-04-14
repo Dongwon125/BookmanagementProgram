c3ab2f75346454ebd0804dc64e877dffe50b859f
DongwonKim <rnforqor27@naver.com>
2023-04-14 15:43:55 +0900

    Menu

--- Bookmanagement/src/Bookmanagement1/MenuManagement.java
+++ Bookmanagement/src/Bookmanagement1/MenuManagement.java
@@ -1,41 +1,52 @@
 package Bookmanagement1;
-
 import java.util.*;
-
-public class Week1 {
-
-
+public class MenuManagement {
 public static void main(String[] args) {
-	
-
-
-	
-		int num = 0;
 		Scanner input = new Scanner(System.in);
-				
+		BookManagement BookManagement = new BookManagement(input);
+		int num = 0;
+		int PW = 1234;
+		System.out.println("Type the admin PW : ");
+		int Pw= input.nextInt();
+		if (Pw!=PW) {
+			System.out.println("Wrong Access.");
+			return;
+			
+		}
+		
+		
 		while (num !=6) {
+			System.out.println("### Book Management System Menu ###");
 			System.out.println("1. Add books");
 			System.out.println("2. Delete books");
 			System.out.println("3. Edit books");
-			System.out.println("4. view books");
+			System.out.println("4. View books");
 			System.out.println("5. Show a menu");
 			System.out.println("6. Exit");
 			System.out.println("select one number between 1 to 6: ");
 			num = input.nextInt();
 					
 			if (num==1){					
-				System.out.println("Type your student Id : ");
-				int studentId = input.nextInt();
-				System.out.println("Type the book's Title : ");
-				String Title = input.next();
-				System.out.println("Type the book's Id : ");
-				int Author = input.nextInt();
-				System.out.println("Type the Day of the Week today (mon to sun) : ");
-				String Day = input.next();
-				}
+				BookManagement.addBooks();	
 			}
-				
+			else if(num==2) {
+				BookManagement.deleteBooks();
+			}
+			else if(num==3) {
+				BookManagement.editBooks();
+			}
+			else if(num==4) {
+				BookManagement.viewBooks();
+			}
+			else {
+				continue;
+			}
 		}
+	}
+		
+		
+
 }
+
  
 
